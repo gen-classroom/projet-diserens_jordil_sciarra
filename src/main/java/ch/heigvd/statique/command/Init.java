8fe329b5b9754f0dd2cddf8df3b52fac087550d8
LoisDiserens <lois.diserens@hispeed.ch>
2021-03-17 10:27:29 +0100

    Save apres suppression de quelques fonctions de mon pc

--- src/main/java/ch/heigvd/statique/command/Init.java
+++ src/main/java/ch/heigvd/statique/command/Init.java
@@ -1,10 +1,14 @@
 package ch.heigvd.statique.command;
 
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "init", description = "Initialize a static site directory")
 public class Init implements Callable<Integer> {
+
+  @CommandLine.Parameters(index = "0", description = "Path of the root directory for the website") String path;
 
   @Override public Integer call() {
     System.out.printf("init");
