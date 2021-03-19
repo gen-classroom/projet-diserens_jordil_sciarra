866f13fc45a8c5aa41bcab29c6b3644fb004d9e4
Loïs Diserens <lois.diserens@hispeed.ch>
2021-03-19 16:12:43 +0100

    REPARATION EFFECTUEE

--- src/main/java/ch/heigvd/statique/Statique.java
+++ src/main/java/ch/heigvd/statique/Statique.java
@@ -1,10 +1,11 @@
 package ch.heigvd.statique;
 
-import ch.heigvd.statique.command.Build;
 import ch.heigvd.statique.command.Clean;
+import ch.heigvd.statique.command.Build;
 import ch.heigvd.statique.command.Init;
 import ch.heigvd.statique.command.Serve;
 import java.util.concurrent.Callable;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -12,8 +13,9 @@ import picocli.CommandLine.Command;
     name = "statique",
     description = "A brand new static site generator.",
     subcommands = {Init.class, Clean.class, Build.class, Serve.class})
-public class Statique implements Callable<Integer> {
-
+public class Statique implements Callable<Integer>
+{
+  public static final String SEPARATOR = "/";
 
   public static void main(String... args) {
     int exitCode = new CommandLine(new Statique()).execute(args);
--- src/main/java/ch/heigvd/statique/command/Init.java
+++ src/main/java/ch/heigvd/statique/command/Init.java
@@ -1,18 +1,107 @@
 package ch.heigvd.statique.command;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Date;
 import java.util.concurrent.Callable;
 
+import ch.heigvd.statique.Statique;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "init", description = "Initialize a static site directory")
 public class Init implements Callable<Integer> {
 
-  @CommandLine.Parameters(index = "0", description = "Path of the root directory for the website") String path;
+  @CommandLine.Parameters(index = "0", description = "Path of the root directory for the website") String pathStr;
 
-  @Override public Integer call() {
-    System.out.printf("init");
-    return 1;
+  @Override public Integer call()
+  {
+      // Creation du chemin de fichier
+      Path path = Paths.get(pathStr);
+      try
+      {
+          Files.createDirectories(path);
+      }
+      catch (IOException e)
+      {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+      }
+
+      // Creation du fichier de config
+      try
+      {
+          File configFile = new File(path + Statique.SEPARATOR + "config.json");
+          if (configFile.createNewFile())
+          {
+              System.out.println("File created: " + configFile.getName());
+
+              // Ecriture des éléments de base
+              String contenuConfig = "" +
+                      "{\n"+
+                      "    \"url\": \""+ pathStr + "\",\n"+
+                      "    \"Title\": \"Website Generator\",\n"+
+                      "    \"Author(s)\": \"Diserens Loïs, Jordil Kevin, Sciarra Daniel\",\n"+
+                      "    \"Creation date\": \""+ java.time.LocalDate.now() + "\",\n"+
+                      "    \"Dossier des fichiers md:\"./contenuMD\",\n"+
+                      "    \"Dossier du site généré:\"./build\"\n"+
+                      "}";
+
+              FileWriter writer = new FileWriter(configFile.getPath());
+              writer.write(contenuConfig);
+              writer.close();
+          }
+          else
+          {
+              System.out.println("File already exists("+ configFile.getName() +").");
+          }
+      }
+      catch (IOException e)
+      {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+      }
+
+      // Creation de la page d'accueil
+      try
+      {
+          File indexFile = new File(path + Statique.SEPARATOR +"index.md");
+          if (indexFile.createNewFile())
+          {
+              System.out.println("File created: " + indexFile.getName());
+
+              // Ecriture des éléments de base
+              String contenuIndex = "" +
+                      "titre: Acceuil\n" +
+                      "auteur: Diserens Loïs\n" +
+                      "date de creation: 2021-18-03\n" +
+                      "---\n" +
+                      "# Accueil\n" +
+                      "## Bienvenue\n" +
+                      "Bienvenue sur cette page générée automatiquement";
+
+              FileWriter writer = new FileWriter(indexFile.getPath());
+              writer.write(contenuIndex);
+              writer.close();
+
+          }
+          else
+          {
+              System.out.println("File already exists("+ indexFile.getName() +").");
+          }
+      }
+      catch (IOException e)
+      {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+      }
+
+      return 1;
   }
 
 }
