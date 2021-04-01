976b38f245cfbf724a7ac20fa28ed90479d3cfb0
Loïs Diserens <lois.diserens@hispeed.ch>
2021-04-01 15:59:10 +0200

    Correction sur la facon de faire les JSON et leur structure

--- src/main/java/ch/heigvd/statique/Md2Html.java
+++ src/main/java/ch/heigvd/statique/Md2Html.java
@@ -10,8 +10,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
-public final class Md2Html {
-
+public final class Md2Html
+{
     public static void convert(File mdFile, String destinationPath) throws IOException {
 
         if (!FilenameUtils.getExtension(mdFile.getName()).equals("md"))
