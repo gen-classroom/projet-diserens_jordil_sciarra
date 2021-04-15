ceefacf2f2ed7df562bba956c5f54d07188e7d98
Loïs Diserens <lois.diserens@hispeed.ch>
2021-04-15 12:51:04 +0200

    Ajout de la command serve et de ses tests

--- src/main/java/ch/heigvd/statique/command/Serve.java
+++ src/main/java/ch/heigvd/statique/command/Serve.java
@@ -1,14 +1,33 @@
 package ch.heigvd.statique.command;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+
+import ch.heigvd.statique.Statique;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "serve", description = "Serve a static site")
 public class Serve implements Callable<Integer> {
 
-  @Override public Integer call() {
-    System.out.printf("serve");
-    return 1;
+  @CommandLine.Parameters(index = "0", description = "Path of the build directory for the website") Path pathStr;
+
+  @Override public Integer call()
+  {
+      try
+      {
+          File fileToOpen = new File(pathStr + Statique.SEPARATOR + "build" + Statique.SEPARATOR + "index.html");
+          Desktop.getDesktop().open(fileToOpen);
+      } catch (IOException e)
+      {
+          e.printStackTrace();
+      }
+
+      return 0;
   }
 
 }
