395c7c0327e1609c38a67cba7539903d732f6d8d
KevinJordil <kevin.jordil@outlook.com>
2021-06-10 12:47:41 +0200

    fix disable for ci

--- src/test/java/ch/heigvd/statique/command/ServeTest.java
+++ src/test/java/ch/heigvd/statique/command/ServeTest.java
@@ -3,7 +3,7 @@ package ch.heigvd.statique.command;
 import ch.heigvd.statique.Statique;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import picocli.CommandLine;
 
 import java.io.File;
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ServeTest
 {
     @Test
-    @DisabledIfSystemProperty(named = "ci-server", matches = "true")
+    @DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
     public void shouldReturnCorrectExitCode() throws IOException {
         // Créé une arborscence de test
         String pathStr = "./monSite";
